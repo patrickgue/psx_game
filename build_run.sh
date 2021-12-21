61eab29349468172d4b4df733b340c3b320a956f
patrick <me@patrickguenthard.ch>
2021-12-21 21:23:32 +0100

    moved sprites to single asm file

--- build_run.sh
+++ build_run.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+
+PSN00BSDK_LIBS=/usr/local/lib/libpsn00b/
 cmake --build ./build $@ && pcsxr -cdfile build/template.cue
---
+++ data.s
@@ -0,0 +1,24 @@
+.section .data
+
+.global street_crosssection_img
+.type street_crosssection_img @object
+street_crosssection_img:
+	.incbin "../assets/street_crosssection.tim"
+
+
+.global street_straight_img
+.type street_straight_img @object
+street_straight_img:
+	.incbin "../assets/street_straight.tim"
+
+
+.global street_t_img
+.type street_t_img @object
+street_t_img:
+	.incbin "../assets/street_t.tim"
+
+
+.global trees_img
+.type trees_img @object
+trees_img:
+.incbin "../assets/trees.tim"
