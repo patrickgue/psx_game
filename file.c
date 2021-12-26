ff8a64bf4ac14db9fc8b86c4c59c8111747a5393
patrickgue <me@patrickguenthard.ch>
2021-12-26 11:10:32 +0100

    replace symlink with files

--- file.c
+++ file.c
@@ -1,1 +1,35 @@
-../game_engine/file.c
+#include "file.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*************************
+ * mock files and struct *
+ *************************/
+
+struct fake_file
+{
+    FILE       index;
+    char       *buffer;
+    int        buffer_len;
+    int        buffer_pos;
+};
+
+struct fake_file *files = NULL;
+int files_count = 0;
+
+
+
+FILE *ffmemopen(void *buff, int leng, char *mode)
+{
+    if (files == NULL)
+        files = malloc(sizeof(struct fake_file));
+}
+
+void ffread(void *, int, int, FILE*);
+void ffwrite(void *, int, int, FILE*);
+void ffclose(FILE *file)
+{
+    if (file != NULL)
+        free(file);
+}
--- file.h
+++ file.h
@@ -1,1 +1,11 @@
-../game_engine/file.h
+#ifndef _file_h
+#define _file_h
+
+typedef int FILE;
+
+FILE *ffmemopen(void *, int, char *);
+void ffread(void *, int, int, FILE*);
+void ffwrite(void *, int, int, FILE*);
+void ffclose(FILE*);
+
+#endif
